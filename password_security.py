15a49ffddaaefe01caa2099663a2efd28058b58b
Maykon Schots <maykonschots@gmail.com>
2021-03-23 11:01:08 -0300

    Check one password via command line ready

--- password_security.py
+++ password_security.py
@@ -1,3 +1,4 @@
+import sys
 import hashlib
 import requests
 
@@ -12,11 +13,24 @@ def request_api_response(hashed_password):
     else:
         return api_response
 
+def get_pwnd_count(api_hashes_response,password_hash):
+    hash_split = (hashes.split(":") for hashes in api_hashes_response.text.splitlines())
+    for h,count in hash_split:
+        if h == password_hash:
+            return int(count)
+    return 0
+
 def password_checker(password):
     hashed_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     password_begin,password_end = hashed_password[:5],hashed_password[5:]
     response = request_api_response(password_begin)
-    print(response.text)
+    return get_pwnd_count(response,password_end)
 
+def main():
+    number_leaks = password_checker(sys.argv[1])
+    if number_leaks > 0:
+        print(f"Wow! This password has been leaked {number_leaks} times. Try to change it now.")
+    else:
+        print("This password is safe for now. Zero leaks found.")
 
-password_checker("sandro_@12!")
+main()
